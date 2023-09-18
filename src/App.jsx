3f72e409fdccd95ab57456d22b254e16838a39cd
unknown <ratonbci95@gmail.com>
2023-09-18 23:31:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+   
+      <h1 className='text-7xl bg-rose-200 text-center p-4'>Vite + React</h1>
+
+      
+      
+      
+    </>
+  )
+}
+
+export default App
